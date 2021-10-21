c4b0a7aeadf541615c90a36d81afa424b76d2500
sukhitashvili <sukhitashvili.soso@gmail.com>
2021-10-21 14:13:58 +0400

    added visualization function

---
+++ utils/vis_utils.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+from vsdkx.core.structs import Inference
+
+
+def visualize(frame: np.array, inference_obj: Inference):
+    """
+    Function for visualizing inout
+    Args:
+        frame: input frame
+        inference_obj: inference object
+
+    Returns:
+        None
+    """
+    boxes = inference_obj.boxes
+    for box in boxes:
+        frame = cv2.rectangle(frame, tuple(box[:2]), tuple(box[2:]),
+                              (0, 255, 0))
+    cv2.imshow('Input Frame', frame)
+    cv2.waitKey(1)
